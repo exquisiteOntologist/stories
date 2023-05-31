64c324be39a384c81ffcca8c9c45b5bf74cb6a0a
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2023-05-31 13:45:55 +0800

    Site add some copy and graphics

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { LogoAlt } from '@/components/logos/logo-alt'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Me } from '../components/illustrations/me'
+import { AppIcon } from '../components/logos/app-icon'
 import imgScreenshot from '../../public/images/early-screenshot.png'
 import { DownloadButton } from '@/components/buttons/download-button'
 
@@ -18,6 +20,49 @@ export default function Home() {
       <figure className={styles.introImg}>
         <Image src={imgScreenshot} alt="Stories provides a view of the world" width={2792/2} height={1424/2} />
       </figure>
+      <section>
+        <div>
+          <h2>Your Questions Answered</h2>
+          <p>Have information work for you. Your questions and your goals are answered &amp; updated as times change. You don’t have to read the internet yourself.</p>
+        </div>
+        <div>
+          {/*  */}
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Take a Breath &amp; Out</h2>
+          <p>Any word, concept, or context has associated words, concepts, and contexts. You can dive deeper into anything. An article could lead to an academic paper and a video, or a wiki.</p>
+        </div>
+        <div>
+          {/*  */}
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Your Sources</h2>
+          <p>Add your chosen Websites, Subscriptions, and APIs.<br></br>For reading and watching, analysis and search, or all of the above. Automatic updates, refreshing not required.</p>
+        </div>
+        <div>
+          {/*  */}
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Compatible with any desktop OS.</h2>
+          <p>You can use it locally on any major desktop OS, or alternatively leave it autonomously running in a warehouse somewhere while you outside.</p>
+          <p>&nbsp;</p>
+          <p>It’s ultrafast too.</p>
+          <p>&nbsp;</p>
+          <AppIcon />
+        </div>
+        <div className={styles.topCenter}>
+          <Me />
+        </div>
+      </section>
+      <footer>
+        <DownloadButton />
+      </footer>
     </main>
   )
 }
---
+++ site/src/components/illustrations/me/index.tsx
@@ -0,0 +1,3 @@
+export const Me: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 168 87" width="168"><g fill="none" fillRule="evenodd"><path d="M147 17c11.046 0 20 8.954 20 20v5c0 11.046-8.954 20-20 20H50.715L28.703 85V62H21C9.954 62 1 53.046 1 42v-5c0-11.046 8.954-20 20-20" stroke="currentColor"/><text opacity=".951" fontFamily="inherit" fontSize="28" fontWeight="500" fill="currentColor" transform="translate(1)"> <tspan x="28.456" y="29">“I’m me”</tspan> </text></g></svg>
+)

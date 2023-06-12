934fd28a297546bc517a6f403bd2a388f9530bae
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2023-06-12 11:51:11 +0800

    Search command unused variable shush

--- stories/src-tauri/src/commands/search.rs
+++ stories/src-tauri/src/commands/search.rs
@@ -3,7 +3,7 @@
 pub fn search(search_phrase: String) -> Result<chirp::entities::SearchResultsDto, String> {
     let results_res = chirp::actions::search::search(&search_phrase);
 
-    if let Err(e) = results_res {
+    if let Err(_e) = results_res {
         return Err("Failed to search".into());
     }
 

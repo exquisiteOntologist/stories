d183dbc07ff3be204baff635f7355859dff15686
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2023-06-20 20:42:26 +0800

    Slip in collecton view transition tweak

--- stories/src/utilities/animate.ts
+++ stories/src/utilities/animate.ts
@@ -7,7 +7,7 @@ import { AnimatePresence, motion } from "framer-motion";
  * https://www.framer.com/motion/component/
  */
 export const motionProps = {
-    initial:{ opacity: 0, translateX: 0, transition: { duration: 0.0 } },
+    initial:{ opacity: 0, translateX: -5, transition: { duration: 0.0 } },
     animate:{ opacity: 1, translateX: 0, transition: { duration: 0.1, staggerChildren: 0.3 } },
     exit:{ opacity: 0, translateX: -5, transition: { duration: 0.1 } },
 }

4a3786eb8240a3660ca0f655235f916a509bd5be
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2023-11-26 13:54:29 +0800

    Add new refresh button

--- stories/src/components/molecules/listings/refresh-row/index.tsx
+++ stories/src/components/molecules/listings/refresh-row/index.tsx
@@ -3,15 +3,18 @@ import { RefreshRowProps } from './interfaces'
 import { motion } from 'framer-motion'
 
 export const RefreshRow: React.FC<RefreshRowProps> = ({ refreshAction: action, refreshPossibe}) => {
-    const labelText = refreshPossibe ? 'Show more recent' : 'Refresh for no reason'
+    const labelText = 'This collection has fresh content'
 
     return (
-        <motion.article onClick={action} className={`cursor-pointer -ml-2 relative group border-b border-transparent bg-gradient-to-r from-yellow-500 to-transparent dark:text-slate-800 ${refreshPossibe ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-            <h1 className="flex text-base whitespace-nowrap mx-0 px-2 py-2 dark:text-slate-300">
-                <span className="truncate">
+        <motion.nav onClick={action} className={`cursor-pointer w-2/3 max-w-xl px-7 py-2 z-50 fixed bottom-10 left-1/2 -translate-x-1/2 group border-b border-transparent bg-black text-white rounded-full to-transparent dark:text-slate-800 ${refreshPossibe ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+            <h1 className="flex justify-between align-middle items-center text-base leading-none whitespace-nowrap mx-0 dark:text-slate-300">
+                <span className="text-lg truncate">
                     {labelText}
                 </span>
+                <button className="font-medium px-4 py-3 rounded-[3px] bg-orange-300 text-black">
+                    Reveal
+                </button>
             </h1>
-        </motion.article>
+        </motion.nav>
     )
 }

532211605c2eaf1b2a97a5b5bc2aef0a4a3924fb
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2023-12-01 22:13:23 +0800

    Update 'your sources' text

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,12 +1,8 @@
-import Image from 'next/image'
 import styles from './page.module.css'
-import { Me, MeDouble } from '../components/illustrations/me'
+import { MeDouble } from '../components/illustrations/me'
 import { QuestionsAnsweredText } from '../components/illustrations/questions-answered'
 import { AppIcon } from '../components/logos/app-icon'
 import { Chip } from '../components/chip'
-// import imgScreenshot from '../../public/images/early-screenshot.png'
-// import imgScreenshotCards from '../../public/images/early-screenshot-cards.png'
-// import imgScreenshotAddSource from '../../public/images/sources-screenshot.png'
 import { DownloadButton } from '@/components/buttons/download-button'
 import { HeroBg } from '@/components/illustrations/hero-bg'
 import React from 'react'
@@ -25,11 +21,8 @@ export default function Home() {
           <h1>Make sense of our world<span>,</span></h1>
         </div>
         <figure className={styles.introImg}>
-          {/* <Image src={imgScreenshot} alt="Stories provides a view of the world" width={2880/2} height={1424/2} /> */}
-          {/* <img src="/images/early-screenshot.png" alt="Stories provides a view of the world" width={2880/2} height={1424/2} /> */}
           {/* <video src="/videos/stories-new.mp4" poster="/videos/stories-new.jpg" muted autoPlay loop width={1280} height={605}></video> */}
           <video src="/videos/stories-new.mp4" poster="/videos/stories-new.jpg" muted autoPlay loop width={1024} height={544}></video>
-          {/* <Image src={imgScreenshotCards} alt="Stories provides a view of the world" width={3204/2} height={1660/2} /> */}
         </figure>
       </header>
       <section>
@@ -54,15 +47,13 @@ export default function Home() {
       </section>
       <section>
         <div>
-          <h2>Your Sources</h2>
-          <p>Add your chosen Websites, Subscriptions, and APIs.<br></br>For reading and watching, analysis and search, or all of the above. Automatic updates, refreshing not required.</p>
+          <h2>Your Sources are your Program</h2>
+          <p>Add your chosen websites and APIs for your content, and automatically receive content updates.</p>
           <p>&nbsp;</p>
           <p>Viewing content opens original web pages. Publishers rejoice!</p>
           <Chip variant="blue">Read Websites today. More soon.</Chip>{/* <Chip variant="orange">Features coming soon</Chip> */}
         </div>
         <div className={styles.middle}>
-          {/* <Image className={styles.floating} src={imgScreenshotAddSource} alt="Stories provides a view of the world" width={786/2} height={682/2} /> */}
-          {/* <img className={styles.floating} src="/images/sources-screenshot.png" alt="Stories provides a view of the world" width={786/2} height={682/2} /> */}
           <video className={styles.floating} src="/videos/stories-vi-02.mp4" poster="/videos/stories-vi-02.jpg" muted autoPlay loop width={393} height={341}></video>
         </div>
       </section>

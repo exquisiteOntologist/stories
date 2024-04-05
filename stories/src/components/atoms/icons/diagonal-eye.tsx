b585f439059970c020d80e921567721836191c6b
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-04-06 00:14:11 +0800

    Add early versions of new top menu icons

---
+++ stories/src/components/atoms/icons/diagonal-eye.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const IconDiagonalEye: React.FC = () => (
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+        <g fill="currentColor" fillRule="evenodd">
+            <path d="M24 0c0 10.7-1.994 17.383-5.982 20.05C14.03 22.714 8.024 24.031 0 24c.016-8 1.71-14 5.082-18S14.76 0 24 0ZM11.5 7a4.5 4.5 0 1 0 0 9 4.5 4.5 0 0 0 0-9Z" />
+            <circle cx="11.5" cy="11.5" r="2.5" />
+        </g>
+    </svg>
+);
---
+++ stories/src/components/atoms/icons/finger.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const IconFinger: React.FC = () => (
+    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12.5 6a6.5 6.5 0 0 1 6.5 6.5V22a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-9.5A6.5 6.5 0 0 1 12.5 6ZM14 8h-3a3 3 0 0 0-3 3v1a2 2 0 0 0 2 2h5a2 2 0 0 0 2-2v-1a3 3 0 0 0-3-3Z" fill="currentColor" fillRule="evenodd" />
+    </svg>
+);

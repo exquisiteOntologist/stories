38411a3c98b0f96b8c14d6131273cef226d3e9ee
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-04-16 17:05:11 +0800

    Make bookmark icon cuter

--- stories/src/components/atoms/icons/bookmark.tsx
+++ stories/src/components/atoms/icons/bookmark.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const IconBookmark: React.FC = () => (
     <svg width="16" height="24" xmlns="http://www.w3.org/2000/svg">
-        <path d="m15 .5.5 22.052-6.935-1.905a1.5 1.5 0 0 0-1.059-.001l-6.33 2.378L.5 1 15 .5Z" fill="#F0315D" stroke="#DD204B" fillRule="evenodd" />
+        <path d="M14 .5c.414 0 .79.168 1.06.44.272.27.44.646.44 1.06v19.103c0 .414-.168.79-.44 1.06a1.495 1.495 0 0 1-1.592.343l-4.55-1.725a2.5 2.5 0 0 0-1.765-.003l-4.625 1.738c-.388.146-.798.12-1.148-.038a1.495 1.495 0 0 1-.88-1.366V2c0-.414.168-.79.44-1.06C1.21.667 1.585.5 2 .5Z" fill="#F0315D" stroke="#DD204B" fillRule="evenodd" />
     </svg>
 );

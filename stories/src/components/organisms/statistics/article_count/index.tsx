b8d8efe227e97f388b522a38d6d6c5b7791139a4
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-06-07 20:05:42 +0800

    Hide percent change in tip

--- stories/src/components/organisms/statistics/article_count/index.tsx
+++ stories/src/components/organisms/statistics/article_count/index.tsx
@@ -19,13 +19,16 @@ export const ArticleCount: React.FC<ArticleCountProps> = ({ collectionId }) => {
 
     const change = count?.yesterday ? Math.round(((count.today - count.yesterday) / count.yesterday) * 100) : 100;
     const pos: boolean = !!change && change >= 0;
-    const percent = change && <span>({`${pos ? "+" : ""}${change}%`})</span>;
+    // const percent = change && <span>({`${pos ? "+" : ""}${change}%`})</span>;
+    const percent = `${pos ? "+" : ""}${change}%`;
 
     return (
-        <motion.hgroup {...motionProps} className="my-8">
+        <motion.hgroup {...motionProps} className="my-8 cursor-default">
             <h2 className="text-3xl">Today</h2>
             <h2 className="text-6xl">{count?.today ?? <>&nbsp;</>}</h2>
-            <h3 className="text-lg">Articles {change ? percent : ""}</h3>
+            <h3 className="text-lg cursor-help" title={percent}>
+                Articles
+            </h3>
         </motion.hgroup>
     );
 };

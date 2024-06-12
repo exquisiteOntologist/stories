63e7dba5723858394f34806d834221dbac6d8949
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-06-12 18:30:15 +0800

    Add descriptors to top-routes

--- stories/src/components/molecules/listings/listings-container-phrase/index.tsx
+++ stories/src/components/molecules/listings/listings-container-phrase/index.tsx
@@ -7,7 +7,7 @@ import { ListingsContainerPhraseProps } from "./interfaces";
 export const ListingsContainerPhrase: React.FC<ListingsContainerPhraseProps> = ({ className, view, phrases, selectAction }) => (
     <ListingsContainer className={className} view={SettingsLayout.CARDS /* view */}>
         {phrases.map((p) => (
-            <ListingRow key={p.id} id={p.id} title={p.phrase} action={() => console.log("phrase", p)} bold={true} source={{ id: 0, name: "Phrase", url: "", site_url: "", kind: SourceKind.WEB }} />
+            <ListingRow key={p.id} id={p.id} title={`${p.phrase} (${p.total.toLocaleString()})`} action={() => console.log("phrase", p)} bold={true} source={{ id: 0, name: "Phrase", url: "", site_url: "", kind: SourceKind.WEB }} />
         ))}
         {/* {view === SettingsLayout.CARDS} */}
     </ListingsContainer>

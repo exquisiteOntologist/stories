ad9f954aa667053e7bc453538e5c1e06922e8668
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-06-18 19:08:21 +0800

    ListingContainerPhrase stop accessing property that doesn't exist

--- stories/src/components/molecules/listings/listings-container-phrase/index.tsx
+++ stories/src/components/molecules/listings/listings-container-phrase/index.tsx
@@ -4,7 +4,7 @@ import ListingsContainer from "../listings-container";
 import { ListingRow } from "../row";
 import { ListingsContainerPhraseProps } from "./interfaces";
 
-export const ListingsContainerPhrase: React.FC<ListingsContainerPhraseProps> = ({ className, view, phrases, selectAction }) => (
+export const ListingsContainerPhrase: React.FC<ListingsContainerPhraseProps> = ({ className, view, phrases }) => (
     <ListingsContainer className={className} view={SettingsLayout.CARDS /* view */}>
         {phrases.map((p) => (
             <ListingRow key={p.id} id={p.id} title={`${p.phrase} (${p.total.toLocaleString()})`} action={() => console.log("phrase", p)} bold={true} source={{ id: 0, name: "Phrase", url: "", site_url: "", kind: SourceKind.WEB }} />

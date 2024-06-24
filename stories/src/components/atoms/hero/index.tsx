3daa56c7f8652093d2d18b507696d2bb968275b8
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-06-24 19:37:05 +0800

    Hero dark mode

--- stories/src/components/atoms/hero/index.tsx
+++ stories/src/components/atoms/hero/index.tsx
@@ -1,3 +1,3 @@
 import React from "react";
 
-export const Hero: React.FC = () => <div className="w-screen h-[70vh] left-1/2 relative -translate-x-1/2 max-w-none mb-12 bg-[#F2E9DB]"></div>;
+export const Hero: React.FC = () => <div className="w-screen h-[70vh] left-1/2 relative -translate-x-1/2 max-w-none mb-12 bg-[#F2E9DB] dark:bg-[#05050b]"></div>;

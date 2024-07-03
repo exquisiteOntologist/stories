48dba5aca856c8a613490097bd8562460a866935
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-07-03 19:44:54 +0800

    Improve loading animation by introducing scale on the down loop

--- stories/tailwind.config.js
+++ stories/tailwind.config.js
@@ -10,11 +10,11 @@ module.exports = {
             },
             keyframes: {
                 wiggle: {
-                    "0%": { transform: "translateY(0) rotateX(0deg)" },
-                    "25%": { transform: "translateY(-30px) rotateX(90deg)" },
-                    "50%": { transform: "translateY(0) rotateX(180deg)" },
-                    "75%": { transform: "translateY(30px) rotateX(270deg)" },
-                    "100%": { transform: "translateY(0) rotateX(360deg)" },
+                    "0%": { transform: "translateY(0) scale(1) rotateX(0deg)" },
+                    "25%": { transform: "translateY(-30px) scale(0.75) rotateX(90deg)" },
+                    "50%": { transform: "translateY(0) scale(0.5) rotateX(180deg)" },
+                    "75%": { transform: "translateY(30px) scale(0.75) rotateX(270deg)" },
+                    "100%": { transform: "translateY(0) scale(1) rotateX(360deg)" },
                 },
             },
         },

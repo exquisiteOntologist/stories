f9806c585481b0ec60041497b8596ab697e03a2f
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-07-03 17:27:27 +0800

    WIP loading indicator

--- stories/src/components/organisms/loading-indicator/index.tsx
+++ stories/src/components/organisms/loading-indicator/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { useAppSelector } from "../../../redux/hooks";
 import { selectIsLoading } from "../../../redux/features/sessionSlice";
+import { IconLoadingRing } from "../../atoms/icons/icon-loading-ring";
 
 export const LoadingIndicator: React.FC = () => {
     const isLoading = useAppSelector(selectIsLoading);
 
-    if (!isLoading) return null;
-    return <p>Is loading</p>;
+    const spinner = <IconLoadingRing />;
+
+    return spinner;
+
+    // if (!isLoading) return null;
+    // return <p>Is loading</p>;
 };
--- stories/tailwind.config.js
+++ stories/tailwind.config.js
@@ -1,17 +1,26 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,css,scss,html}', './index.html'],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        gray: {
-          100: '#F8F6F6'
-        }
-      }
+    content: ["./src/**/*.{js,jsx,ts,tsx,css,scss,html}", "./index.html"],
+    darkMode: "media", // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    100: "#F8F6F6",
+                },
+            },
+            keyframes: {
+                wiggle: {
+                    "0%": { transform: "translateY(0) rotateX(0deg)" },
+                    "25%": { transform: "translateY(-30px) rotateX(90deg)" },
+                    "50%": { transform: "translateY(0) rotateX(180deg)" },
+                    "75%": { transform: "translateY(30px) rotateX(270deg)" },
+                    "100%": { transform: "translateY(0) rotateX(360deg)" },
+                },
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};

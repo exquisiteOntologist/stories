f7b9d6f5190062ae417a80579919fa8ec02bf5ca
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-07-06 14:34:18 +0800

    Style bookmark on filter button when active

--- stories/src/components/molecules/filter-button/index.tsx
+++ stories/src/components/molecules/filter-button/index.tsx
@@ -5,7 +5,7 @@ import { FilterButtonProps } from "./interfaces";
 
 export const FilterButton: React.FC<FilterButtonProps> = ({ action, active, number, colour }) => (
     <button className={`flex align-middle items-center ${active ? "bg-current" : "bg-transparent"} border-2 border-current rounded-md px-4 py-3 mr-4 hover:border-current`} style={{ color: colour }} onClick={action}>
-        <IconBookmark active={!active} />
+        <IconBookmark active={!active} strokeColour="white" />
         <span className={`text-lg ${active ? "text-white" : "text-current"} font-bold ml-20`}>{number}</span>
     </button>
 );

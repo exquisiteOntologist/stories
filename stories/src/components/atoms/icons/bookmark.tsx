3e9dde823e9982e236a9381e638e26707172a160
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-07-06 18:09:29 +0800

    Fix bookmark fill colour

--- stories/src/components/atoms/icons/bookmark.tsx
+++ stories/src/components/atoms/icons/bookmark.tsx
@@ -3,7 +3,7 @@ import { FilterButtonIconProps } from "./interfaces";
 
 interface IconBookmarkProps extends FilterButtonIconProps {}
 
-export const IconBookmark: React.FC<IconBookmarkProps> = ({ filled, fillColour = "white", strokeColour = "#C6B3B8" }) => (
+export const IconBookmark: React.FC<IconBookmarkProps> = ({ filled, fillColour = "transparent", strokeColour = "#C6B3B8" }) => (
     <svg width="16" height="24" viewBox="0 0 16 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M14 .5c.414 0 .79.168 1.06.44.272.27.44.646.44 1.06v19.103c0 .414-.168.79-.44 1.06a1.495 1.495 0 0 1-1.592.343l-4.55-1.725a2.5 2.5 0 0 0-1.765-.003l-4.625 1.738c-.388.146-.798.12-1.148-.038a1.495 1.495 0 0 1-.88-1.366V2c0-.414.168-.79.44-1.06C1.21.667 1.585.5 2 .5Z" fill={filled ? "#F0315D" : fillColour} stroke={filled ? "#DD204B" : strokeColour} strokeWidth={filled ? 1 : 2} fillRule="evenodd" />
     </svg>
--- stories/src/components/molecules/filter-button/index.tsx
+++ stories/src/components/molecules/filter-button/index.tsx
@@ -3,7 +3,7 @@ import { FilterButtonProps } from "./interfaces";
 
 export const FilterButton: React.FC<FilterButtonProps> = ({ action, active, number, colour, Icon }) => (
     <button className={`flex align-middle items-center ${active ? "bg-current" : "bg-transparent"} border-2 border-current rounded-md px-4 py-3 mr-4 hover:border-current`} style={{ color: colour }} onClick={action}>
-        <Icon filled={!active} strokeColour="white" />
+        <Icon filled={!active} strokeColour="white" fillColour="white" />
         <span className={`text-lg ${active ? "text-white" : "text-current"} font-bold ml-20`}>{number}</span>
     </button>
 );

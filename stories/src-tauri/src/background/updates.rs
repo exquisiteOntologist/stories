322d7a0a091cbb633ad672c31a05d470d888b07a
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-08-22 17:26:59 +0800

    Remove unused crate

--- stories/src-tauri/src/background/updates.rs
+++ stories/src-tauri/src/background/updates.rs
@@ -1,18 +1,12 @@
 use std::time::Duration;
 
 use tokio::spawn;
-use tokio_schedule::{every, Job};
 
 const MIN_INTERVAL: Duration = Duration::from_secs(30);
 
 pub async fn update() {
     _ = chirp::actions::update::update().await;
 }
-
-// pub async fn panicky() {
-//     eprintln!("Oh no, I'm going to panic!");
-//     panic!();
-// }
 
 pub async fn continual_updates() -> Box<dyn 'static + Send> {
     loop {
@@ -23,16 +17,3 @@ pub async fn continual_updates() -> Box<dyn 'static + Send> {
         println!("after sleep");
     }
 }
-
-// pub async fn replicating_update_schedule() {
-//     update().await;
-//     tokio::time::sleep(MIN_INTERVAL).await;
-//     tokio::spawn(replicating_update_schedule());
-//      not returning a value that implements Send (not returning a value)
-// }
-
-// pub async fn scheduled_updates() {
-//     tokio::spawn(update());
-//     let schedule = every(30).minutes().perform(update);
-//     spawn(schedule);
-// }

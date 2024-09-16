1e7e843b11a81eced4b73d551274a09af1c90d09
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-09-16 15:14:02 +0800

    Remove console statements from ImageCanvas

--- stories/src/components/atoms/image-canvas/index.tsx
+++ stories/src/components/atoms/image-canvas/index.tsx
@@ -10,30 +10,21 @@ export const ImageCanvas: React.FC<ImageCanvasProps> = ({ className, src, style 
     useEffect(() => {
         (async () => {
             if (rendered || img || !src) return
-            console.log('fetching img', src)
             const response = await fetch(src, {
                 method: 'GET',
                 connectTimeout: 30000,
                 // proxy
                 cache: 'force-cache'
             }).catch(e => console.error('e', e));
-            console.log('response for image', response, src)
-            if (!response || !response.blob || !response.arrayBuffer) return
+            if (!response || !response.blob) return
             const blob = await response.blob()
-            // const buffer = await response.arrayBuffer()
             const bitmap = await createImageBitmap(blob)
-            // const img = new Image(bitmap.width, bitmap.height)
-            console.log('bitmap', bitmap.width, bitmap.height)
             setImg(bitmap)
-   
         })()
     }, [img])
 
     useEffect(() => {
-        console.log('render cycle', img)
-
         if (rendered || !img || !canvasRef.current) return
-        console.log('drawing image')
 
         canvasRef.current.width = img.width
         canvasRef.current.height = img.height
@@ -43,13 +34,9 @@ export const ImageCanvas: React.FC<ImageCanvasProps> = ({ className, src, style 
         ctx.fillStyle = '#fff'
         ctx?.fillRect(0, 0, img.width, img.height)
         ctx?.drawImage(img, 0, 0, img.width, img.height)
-        // ctx?.transferFromImageBitmap(img)
         setRendered(true)
         img.close()
-        console.log('img has been drawn', img)
     }, [img])
-
-    console.log('hello', img)
 
     return (
         <canvas className={className} ref={canvasRef} style={style} />

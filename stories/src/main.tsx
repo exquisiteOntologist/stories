60bb2ebccff26dbcd55ebf6854cc538d26a57f86
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-04-13 16:55:48 +0800

    Separate tailwind css import from global.scss

--- stories/src/main.tsx
+++ stories/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppView from "./views";
+import "./styles/wind.css";
 import "./styles/global.scss";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
@@ -14,26 +15,26 @@ dayjs.extend(relativeTime);
 dayjs.extend(updateLocale);
 
 dayjs.updateLocale("en", {
-    relativeTime: {
-        future: "in %s",
-        past: "%s",
-        s: "a few secs",
-        m: "1min",
-        mm: "%dmins",
-        h: "1hr",
-        hh: "%dhrs",
-        d: "1d",
-        dd: "%dd",
-        M: "1m",
-        MM: "%dm",
-        y: "1yr",
-        yy: "%dyrs",
-    },
+  relativeTime: {
+    future: "in %s",
+    past: "%s",
+    s: "a few secs",
+    m: "1min",
+    mm: "%dmins",
+    h: "1hr",
+    hh: "%dhrs",
+    d: "1d",
+    dd: "%dd",
+    M: "1m",
+    MM: "%dm",
+    y: "1yr",
+    yy: "%dyrs",
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    // <React.StrictMode>
-    //   <AppView />
-    // </React.StrictMode>
-    <AppView />,
+  // <React.StrictMode>
+  //   <AppView />
+  // </React.StrictMode>
+  <AppView />,
 );

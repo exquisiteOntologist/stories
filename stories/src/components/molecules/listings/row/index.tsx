9a228b4bedbb3169ae376578202ccb7ac8e81931
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-05-18 15:27:38 +0800

    Prevent long titles in list view widening viewport

--- stories/src/components/molecules/listings/row/index.tsx
+++ stories/src/components/molecules/listings/row/index.tsx
@@ -7,55 +7,74 @@ import { RelativeDate } from "../../../atoms/relative-date";
 import { Bookmark } from "../../bookmark";
 import { SettingsLayout } from "../../../../data/chirp-types";
 
-export const ListingRow: React.FC<ListingRowProps> = ({ title, linkUrl, action, content, source, bold }) => {
-    // const description = content && (
-    //     <span className='font-normal ml-6 text-gray-300'>
-    //         {/* {content?.description?.slice(0, 100)} */}
-    //     </span>
-    // )
+export const ListingRow: React.FC<ListingRowProps> = ({
+  title,
+  linkUrl,
+  action,
+  content,
+  source,
+  bold,
+}) => {
+  // const description = content && (
+  //     <span className='font-normal ml-6 text-gray-300'>
+  //         {/* {content?.description?.slice(0, 100)} */}
+  //     </span>
+  // )
 
-    const actionInner = (
-        <>
-            {title}
-            {/* {description} */}
-        </>
-    );
+  const actionInner = (
+    <>
+      {title}
+      {/* {description} */}
+    </>
+  );
 
-    // top-1/2 -translate-y-1/2
-    // transition-all duration-75
-    const actionBookmark = content && linkUrl && <Bookmark content={content} layout={SettingsLayout.ROWS} />;
+  // top-1/2 -translate-y-1/2
+  // transition-all duration-75
+  const actionBookmark = content && linkUrl && (
+    <Bookmark content={content} layout={SettingsLayout.ROWS} />
+  );
 
-    const titleInner = linkUrl ? (
-        <a className="block font-bold" href={linkUrl} target="_blank">
-            {actionInner}
-        </a>
-    ) : (
-        action && (
-            <span className="block cursor-pointer font-bold" onClick={action}>
-                {actionInner}
-            </span>
-        )
-    );
+  const titleClasses =
+    "block font-bold cursor-pointer whitespace-nowrap overflow-hidden text-ellipsis";
 
-    const nodeRecency = content?.date_published && (
-        <span className="ml-2 text-sm text-gray-300 dark:text-gray-500 select-none cursor-default">
-            <RelativeDate date={content?.date_published} />
-        </span>
-    );
+  const titleInner = linkUrl ? (
+    <a className={titleClasses} href={linkUrl} target="_blank">
+      {actionInner}
+    </a>
+  ) : (
+    action && (
+      <span className={titleClasses} onClick={action}>
+        {actionInner}
+      </span>
+    )
+  );
 
-    const nodeTitleLink = (
-        <span className="truncate">
-            {titleInner}
-            <SourceLink source={source} isBlock={false} />
-            {nodeRecency}
-        </span>
-    );
+  const nodeRecency = content?.date_published && (
+    <span className="ml-2 text-sm text-gray-300 dark:text-gray-500 select-none cursor-default">
+      <RelativeDate date={content?.date_published} />
+    </span>
+  );
 
-    // the pseudo-element helps capture the mouse so the user can seamlessly move to the bookmark icon
-    return (
-        <motion.article {...motionProps} className="group relative bg-transparent border-gray-100 dark:border-slate-800 before:block before:absolute before:z-0 before:-inset-0 before:-left-2">
-            {actionBookmark}
-            <h1 className={`flex text-base ${bold ? "font-bold" : ""} mx-0 ${source ? "my-4" : "my-2"} relative z-10 whitespace-nowrap dark:text-slate-400 select-none`}>{nodeTitleLink}</h1>
-        </motion.article>
-    );
+  const nodeTitleLink = (
+    <span className="truncate">
+      {titleInner}
+      <SourceLink source={source} isBlock={false} />
+      {nodeRecency}
+    </span>
+  );
+
+  // the pseudo-element helps capture the mouse so the user can seamlessly move to the bookmark icon
+  return (
+    <motion.article
+      {...motionProps}
+      className="group relative bg-transparent border-gray-100 dark:border-slate-800 before:block before:absolute before:z-0 before:-inset-0 before:-left-2"
+    >
+      {actionBookmark}
+      <h1
+        className={`flex text-base ${bold ? "font-bold" : ""} mx-0 ${source ? "my-4" : "my-2"} relative z-10 whitespace-nowrap dark:text-slate-400 select-none`}
+      >
+        {nodeTitleLink}
+      </h1>
+    </motion.article>
+  );
 };
--- stories/src/views/index.tsx
+++ stories/src/views/index.tsx
@@ -9,67 +9,76 @@ import { selectColours } from "../redux/features/themeSlice";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { ReduxWrapper } from "../redux/redux-wrapper";
 import { setBodyBackground } from "../utilities/graphics/colours";
-import { routeCollectionView, routeSearch, routeSourcesEdit } from "../data/top-routes";
+import {
+  routeCollectionView,
+  routeSearch,
+  routeSourcesEdit,
+} from "../data/top-routes";
 import { selectSession, ticker } from "../redux/features/sessionSlice";
 import { useEffect } from "react";
 
 const AppPageInner: React.FC = () => {
-    const dispatch = useAppDispatch();
-    const { ticking } = useAppSelector(selectSession);
-    const colours = useAppSelector(selectColours);
-    const elMainStyle: React.CSSProperties = {
-        "--pixel-ratio": window?.devicePixelRatio || 1,
-        "--primary": colours.primaryLightnessAdjusted,
-        background: colours.backgroundLightnessAdjusted,
-    } as React.CSSProperties;
+  const dispatch = useAppDispatch();
+  const { ticking } = useAppSelector(selectSession);
+  const colours = useAppSelector(selectColours);
+  const elMainStyle: React.CSSProperties = {
+    "--pixel-ratio": window?.devicePixelRatio || 1,
+    "--primary": colours.primaryLightnessAdjusted,
+    background: colours.backgroundLightnessAdjusted,
+  } as React.CSSProperties;
 
-    setBodyBackground(colours.backgroundLightnessAdjusted ?? undefined);
+  setBodyBackground(colours.backgroundLightnessAdjusted ?? undefined);
 
-    const routerClassNames = "flex justify-center py-12 overflow-x-hidden";
+  const routerClassNames = "flex justify-center py-12 px-8 overflow-x-hidden";
 
-    const element = useRoutes([
-        {
-            path: routeCollectionView,
-            element: <CollectionView />,
-        },
-        {
-            path: routeSearch,
-            element: <CollectionSearchView />,
-        },
-        {
-            path: routeSourcesEdit,
-            element: <CollectionEditView />,
-        },
-    ]);
+  const element = useRoutes([
+    {
+      path: routeCollectionView,
+      element: <CollectionView />,
+    },
+    {
+      path: routeSearch,
+      element: <CollectionSearchView />,
+    },
+    {
+      path: routeSourcesEdit,
+      element: <CollectionEditView />,
+    },
+  ]);
 
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        if (!ticking) dispatch(ticker());
-    }, [dispatch]);
+  useEffect(() => {
+    if (!ticking) dispatch(ticker());
+  }, [dispatch]);
 
-    if (!element) return null;
+  if (!element) return null;
 
-    // scrollTo(0,0)
+  // scrollTo(0,0)
 
-    return (
-        <main className="w-screen flex flex-col min-h-screen overscroll-none transition-all duration-1000" style={elMainStyle}>
-            <AppHeader location={location} />
-            <div className={routerClassNames}>
-                <AnimatePresence mode="wait">{React.cloneElement(element, { key: location.pathname })}</AnimatePresence>
-            </div>
-        </main>
-    );
+  return (
+    <main
+      className="w-screen flex flex-col min-h-screen overscroll-none transition-all duration-1000"
+      style={elMainStyle}
+    >
+      <AppHeader location={location} />
+      <div className={routerClassNames}>
+        <AnimatePresence mode="wait">
+          {React.cloneElement(element, { key: location.pathname })}
+        </AnimatePresence>
+      </div>
+    </main>
+  );
 };
 
 const AppView: React.FC = () => (
-    <React.Fragment>
-        <ReduxWrapper>
-            <BrowserRouter>
-                <AppPageInner />
-            </BrowserRouter>
-        </ReduxWrapper>
-    </React.Fragment>
+  <React.Fragment>
+    <ReduxWrapper>
+      <BrowserRouter>
+        <AppPageInner />
+      </BrowserRouter>
+    </ReduxWrapper>
+  </React.Fragment>
 );
 
 export default AppView;

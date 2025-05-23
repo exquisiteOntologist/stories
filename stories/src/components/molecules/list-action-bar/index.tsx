0b802a414448ce929bc5e6cf457f6a683576ea2d
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-05-23 12:13:18 +0800

    Fix opacity for collection-customizer and list-action-bar

--- stories/src/components/molecules/list-action-bar/index.tsx
+++ stories/src/components/molecules/list-action-bar/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Button } from '../../atoms/button'
-import { IconRemove } from '../../atoms/icons/remove'
-import { ListActionBarProps } from './interface'
+import React from "react";
+import { Button } from "../../atoms/button";
+import { IconRemove } from "../../atoms/icons/remove";
+import { ListActionBarProps } from "./interface";
 
-export const ListActionBar: React.FC<ListActionBarProps> = ({ show, deleteAction }) => (
-    <div className={`flex justify-center my-5 transition-all duration-150 ${show ? 'opacity-1 translate-y-0' : 'opacity-0 translate-y-2'}`}>
-        <Button 
-            Icon={IconRemove}
-            action={deleteAction}
-        />
-    </div>
-)
+export const ListActionBar: React.FC<ListActionBarProps> = ({
+  show,
+  deleteAction,
+}) => (
+  <div
+    className={`flex justify-center my-5 transition-all duration-150 ${show ? "opacity-100 translate-y-0" : "opacity-0 translate-y-2"}`}
+  >
+    <Button Icon={IconRemove} action={deleteAction} />
+  </div>
+);
--- stories/src/components/organisms/collection-customizer/index.tsx
+++ stories/src/components/organisms/collection-customizer/index.tsx
@@ -11,38 +11,61 @@ import { setCollectionSettings } from "../../../redux/features/collectionSetting
 import { CollectionCustomizerProps } from "./interfaces";
 import { setIsCustomizing } from "../../../redux/features/navSlice";
 import { PopoverViewMode } from "../../molecules/popovers/popover-view-mode";
-import { selectContentByRecency, selectContentOfCollection } from "../../../redux/features/contentsSlice";
+import {
+  selectContentByRecency,
+  selectContentOfCollection,
+} from "../../../redux/features/contentsSlice";
 import { sourcesSelectors } from "../../../redux/features/sourcesSlice";
 
-export const CollectionCustomizer: React.FC<CollectionCustomizerProps> = ({ collectionSettings, isCustomizing }) => {
-    const dispatch = useAppDispatch();
-    const previewContents = useAppSelector((s) => selectContentByRecency(s, 20));
-    const sources = useAppSelector(sourcesSelectors.selectAll);
+export const CollectionCustomizer: React.FC<CollectionCustomizerProps> = ({
+  collectionSettings,
+  isCustomizing,
+}) => {
+  const dispatch = useAppDispatch();
+  const previewContents = useAppSelector((s) => selectContentByRecency(s, 20));
+  const sources = useAppSelector(sourcesSelectors.selectAll);
 
-    const viewIsList = collectionSettings?.layout === SettingsLayout.ROWS;
-    const otherLayoutOption = viewIsList ? SettingsLayout.CARDS : SettingsLayout.ROWS;
+  const viewIsList = collectionSettings?.layout === SettingsLayout.ROWS;
+  const otherLayoutOption = viewIsList
+    ? SettingsLayout.CARDS
+    : SettingsLayout.ROWS;
 
-    return (
-        <div className={`z-50 transition-all duration-100 ${isCustomizing ? "opacity-1 translate-x-0 translate-y-0" : "opacity-0 translate-x-3 -translate-y-3"} mb-6`}>
-            <div className={`flex justify-start`}>
-                <Button label="Done" Icon={IconTickCircle} action={() => dispatch(setIsCustomizing(false))} />
-                <Button
-                    label={`View as ${viewIsList ? "Cards" : "List"}`}
-                    Icon={viewIsList ? IconGrid : IconList}
-                    action={() =>
-                        dispatch(
-                            setCollectionSettings({
-                                ...collectionSettings,
-                                layout: otherLayoutOption,
-                            } as CollectionSettings),
-                        )
-                    }
-                >
-                    <PopoverViewMode contents={previewContents} sources={sources} layout={otherLayoutOption} />
-                </Button>
-                <Button label="Add Widget" Icon={IconAddCircle} action={() => void 8} disabled={true} />
-                <Button Icon={IconShapes} label="Sources" linkTo={`/edit`} />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`z-50 transition-all duration-100 ${isCustomizing ? "opacity-100 translate-x-0 translate-y-0" : "opacity-0 translate-x-3 -translate-y-3"} mb-6`}
+    >
+      <div className={`flex justify-start`}>
+        <Button
+          label="Done"
+          Icon={IconTickCircle}
+          action={() => dispatch(setIsCustomizing(false))}
+        />
+        <Button
+          label={`View as ${viewIsList ? "Cards" : "List"}`}
+          Icon={viewIsList ? IconGrid : IconList}
+          action={() =>
+            dispatch(
+              setCollectionSettings({
+                ...collectionSettings,
+                layout: otherLayoutOption,
+              } as CollectionSettings),
+            )
+          }
+        >
+          <PopoverViewMode
+            contents={previewContents}
+            sources={sources}
+            layout={otherLayoutOption}
+          />
+        </Button>
+        <Button
+          label="Add Widget"
+          Icon={IconAddCircle}
+          action={() => void 8}
+          disabled={true}
+        />
+        <Button Icon={IconShapes} label="Sources" linkTo={`/edit`} />
+      </div>
+    </div>
+  );
 };

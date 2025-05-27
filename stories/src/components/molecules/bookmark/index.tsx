b9b4988a5bdc2fa71c97c88fbd66c77929c054f6
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-05-27 16:19:04 +0800

    Fix hidden bookmarks. (from Tailwind opacity 1 becoming 100)

--- stories/src/components/molecules/bookmark/index.tsx
+++ stories/src/components/molecules/bookmark/index.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import { IconBookmark } from "../../atoms/icons/bookmark";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import { addMark, marksSelectors, removeMark } from "../../../redux/features/marksSlice";
+import {
+  addMark,
+  marksSelectors,
+  removeMark,
+} from "../../../redux/features/marksSlice";
 import { ContentDto, SettingsLayout } from "../../../data/chirp-types";
 
 export interface BookmarkProps {
-    content: ContentDto;
-    layout: SettingsLayout;
+  content: ContentDto;
+  layout: SettingsLayout;
 }
 
 export const Bookmark: React.FC<BookmarkProps> = ({ content, layout }) => {
-    const dispatch = useAppDispatch();
-    const isBookmarked: boolean = typeof useAppSelector((s) => marksSelectors.selectById(s, content.id)) === "number";
-    const actionToggleMark = () => dispatch(isBookmarked ? removeMark(content) : addMark(content));
-    const layoutStyles = layout === SettingsLayout.CARDS ? "absolute top-0 left-0 p-2" : "absolute -left-6 right-full pr-2";
+  const dispatch = useAppDispatch();
+  const isBookmarked: boolean =
+    typeof useAppSelector((s) => marksSelectors.selectById(s, content.id)) ===
+    "number";
+  const actionToggleMark = () =>
+    dispatch(isBookmarked ? removeMark(content) : addMark(content));
+  const layoutStyles =
+    layout === SettingsLayout.CARDS
+      ? "absolute top-0 left-0 p-2"
+      : "absolute -left-6 right-full pr-2";
 
-    return (
-        <button className={`${layoutStyles} bg-transparent text-rose-500 ${isBookmarked ? "opacity-1" : "opacity-0"} group-hover:opacity-100`} onClick={actionToggleMark}>
-            <IconBookmark filled={isBookmarked} />
-        </button>
-    );
+  return (
+    <button
+      className={`${layoutStyles} bg-transparent text-rose-500 ${isBookmarked ? "opacity-100" : "opacity-0"} group-hover:opacity-100`}
+      onClick={actionToggleMark}
+    >
+      <IconBookmark filled={isBookmarked} />
+    </button>
+  );
 };

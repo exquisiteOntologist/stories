c1159de3af41fdeddbfc9b03fa7afa328c0a2fb1
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-11-15 17:26:39 +0800

    Cleanup filter-button

--- stories/src/components/molecules/filter-button/index.tsx
+++ stories/src/components/molecules/filter-button/index.tsx
@@ -1,39 +1,11 @@
 import React from "react";
 import { FilterButtonProps } from "./interfaces";
-import { ListingRow } from "../listings/row";
-import { Surface } from "../../atoms/surface";
 import { PillButton } from "../../atoms/pill-button";
-
-// export const FilterButton: React.FC<FilterButtonProps> = ({ action, active, number, colour, Icon, label }) => (
-//     <button className={`flex align-middle items-center ${active ? "bg-current" : "bg-transparent"} border-2 border-gray-100 dark:border-slate-800 rounded-md px-4 py-3 mr-4 hover:border-current`} style={{ color: colour }} onClick={action}>
-//         <Icon filled={!active} strokeColour="white" fillColour="white" />
-//         <span className={`text-lg ${active ? "text-white" : "text-current"} font-bold ml-4`}>{number} {label}</span>
-//     </button>
-// );
-
-// export const FilterButton: React.FC<FilterButtonProps> = ({
-//   action,
-//   active,
-//   number,
-//   colour,
-//   Icon,
-//   label,
-// }) => (
-//   <ListingRow
-//     id={label}
-//     title={label}
-//     action={action}
-//     hasBg={true}
-//     trailingIcon={
-//       <Icon filled={!active} strokeColour="white" fillColour="white" />
-//     }
-//   />
-// );
 
 export const FilterButton: React.FC<FilterButtonProps> = ({
   action,
   active,
-  number,
+  // number,
   activeClass,
   Icon,
   label,
@@ -43,21 +15,6 @@ export const FilterButton: React.FC<FilterButtonProps> = ({
     active={active}
     activeClass={activeClass}
     Icon={Icon}
-    label={`${label}`}
+    label={label}
   />
 );
-
-// export const FilterButton: React.FC<FilterButtonProps> = ({
-//   action,
-//   active,
-//   number,
-//   colour,
-//   Icon,
-//   label,
-// }) => (
-//   <button onClick={action}>
-//     <Surface Tag="div" bold={true} hasBg={true}>
-//       {label}
-//     </Surface>
-//   </button>
-// );

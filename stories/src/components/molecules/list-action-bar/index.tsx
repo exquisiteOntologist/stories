43e88fc149a196aa127c32065c14aed61f9132d7
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-11-15 11:20:33 +0800

    Rename Button to FloatingButton

--- stories/src/components/molecules/list-action-bar/index.tsx
+++ stories/src/components/molecules/list-action-bar/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "../../atoms/button";
+import { FloatingButton } from "../../atoms/floating-button";
 import { IconRemove } from "../../atoms/icons/remove";
 import { ListActionBarProps } from "./interface";
 
@@ -10,6 +10,6 @@ export const ListActionBar: React.FC<ListActionBarProps> = ({
   <div
     className={`flex justify-center my-5 transition-all duration-150 ${show ? "opacity-100 translate-y-0" : "opacity-0 translate-y-2"}`}
   >
-    <Button Icon={IconRemove} action={deleteAction} />
+    <FloatingButton Icon={IconRemove} action={deleteAction} />
   </div>
 );
--- stories/src/components/organisms/app-header/index.tsx
+++ stories/src/components/organisms/app-header/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Navigate, Location, useNavigate } from "react-router-dom";
-import { routeCollectionView, routeSearch, routeSourcesEdit } from "../../../data/top-routes";
-import { Button } from "../../atoms/button";
+import {
+  routeCollectionView,
+  routeSearch,
+  routeSourcesEdit,
+} from "../../../data/top-routes";
+import { FloatingButton } from "../../atoms/floating-button";
 // import { IconEllipsis } from "../../atoms/icons/ellipsis";
 // import { IconPaintRoller } from "../../atoms/icons/paint-roller";
 // import { IconSearch } from "../../atoms/icons/search";
@@ -11,7 +15,13 @@ import { ShutEye } from "../../atoms/logo/shut-eye";
 import { ButtonsGroup } from "../../molecules/buttons-group";
 import { SearchShortcutHandlers } from "../../alternate/search-shortcut-handler";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import { chooseCollection, selectHistory, selectIsCustomizing, setIsCustomizing, toggleIsCustomizing } from "../../../redux/features/navSlice";
+import {
+  chooseCollection,
+  selectHistory,
+  selectIsCustomizing,
+  setIsCustomizing,
+  toggleIsCustomizing,
+} from "../../../redux/features/navSlice";
 import { IconFinger } from "../../atoms/icons/finger";
 import { IconDisembodied } from "../../atoms/icons/disembodied";
 import { IconDiagonalEye } from "../../atoms/icons/diagonal-eye";
@@ -21,122 +31,138 @@ import { IconOsCheek } from "../../atoms/icons/oscheek";
 const scrollToTop = () => scrollTo({ top: 0 });
 
 export interface AppMenuNavigationProps {
-    location?: Location;
+  location?: Location;
 }
 
 const AppMenuNavigation: React.FC<AppMenuNavigationProps> = ({ location }) => {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const submergeHistoryIds = useAppSelector(selectHistory);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const submergeHistoryIds = useAppSelector(selectHistory);
 
-    return (
-        <nav className="pointer-events-auto py-4">
-            <Button
-                Icon={ShutEye}
-                label=""
-                action={() => {
-                    dispatch(setIsCustomizing(false));
+  return (
+    <nav className="pointer-events-auto py-4">
+      <FloatingButton
+        Icon={ShutEye}
+        label=""
+        action={() => {
+          dispatch(setIsCustomizing(false));
 
-                    if (location?.pathname === routeCollectionView) {
-                        if (submergeHistoryIds.length <= 1) return;
+          if (location?.pathname === routeCollectionView) {
+            if (submergeHistoryIds.length <= 1) return;
 
-                        const priorId = submergeHistoryIds[submergeHistoryIds.length - 2];
+            const priorId = submergeHistoryIds[submergeHistoryIds.length - 2];
 
-                        dispatch(chooseCollection(priorId));
-                    } else {
-                        navigate(routeCollectionView);
-                    }
-                }}
-                usePadding={false}
-                sideAction={() => {
-                    const alreadyAtDest = location?.pathname === routeCollectionView;
-                    if (alreadyAtDest) scrollToTop();
-                }}
-            />
-        </nav>
-    );
+            dispatch(chooseCollection(priorId));
+          } else {
+            navigate(routeCollectionView);
+          }
+        }}
+        usePadding={false}
+        sideAction={() => {
+          const alreadyAtDest = location?.pathname === routeCollectionView;
+          if (alreadyAtDest) scrollToTop();
+        }}
+      />
+    </nav>
+  );
 };
 
 const AppMenuActions: React.FC<AppHeaderProps> = ({ location }) => {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const isCustomizing = useAppSelector(selectIsCustomizing);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const isCustomizing = useAppSelector(selectIsCustomizing);
 
-    console.log("is custom", isCustomizing);
+  console.log("is custom", isCustomizing);
 
-    return (
-        <ButtonsGroup className="pointer-events-auto grow justify-end py-4 pl-20" expands={true} IconExpand={IconOsCheek} leftward={true}>
-            <Button
-                Icon={IconDiagonalEye}
-                label="Style"
-                action={() => {
-                    const needScroll = !isCustomizing;
+  return (
+    <ButtonsGroup
+      className="pointer-events-auto grow justify-end py-4 pl-20"
+      expands={true}
+      IconExpand={IconOsCheek}
+      leftward={true}
+    >
+      <FloatingButton
+        Icon={IconDiagonalEye}
+        label="Style"
+        action={() => {
+          const needScroll = !isCustomizing;
 
-                    if (location?.pathname === routeCollectionView) {
-                        dispatch(toggleIsCustomizing());
-                    } else {
-                        dispatch(setIsCustomizing(true));
-                        navigate(routeCollectionView);
-                    }
+          if (location?.pathname === routeCollectionView) {
+            dispatch(toggleIsCustomizing());
+          } else {
+            dispatch(setIsCustomizing(true));
+            navigate(routeCollectionView);
+          }
 
-                    if (needScroll) scrollToTop();
-                }}
-            />
-            <Button
-                Icon={IconDisembodied}
-                label="Contents"
-                // linkTo={`${location.pathname}/edit`}
-                linkTo={routeSourcesEdit}
-            />
-            {/* <Button
+          if (needScroll) scrollToTop();
+        }}
+      />
+      <FloatingButton
+        Icon={IconDisembodied}
+        label="Contents"
+        // linkTo={`${location.pathname}/edit`}
+        linkTo={routeSourcesEdit}
+      />
+      {/* <FloatingButton
                 Icon={IconWidgets}
                 label="Widgets"
                 linkTo="/"
                 disabled={true}
             /> */}
-            {/* <Button
+      {/* <FloatingButton
                 Icon={IconMagic}
                 label="Magic ID"
                 action={() => console.log('magic ID')}
                 disabled={true}
             /> */}
-            <Button Icon={IconFinger} PopoverIcon={ShortcutCommandF} label="Search" linkTo={routeSearch} disabled={false} />
-        </ButtonsGroup>
-    );
+      <FloatingButton
+        Icon={IconFinger}
+        PopoverIcon={ShortcutCommandF}
+        label="Search"
+        linkTo={routeSearch}
+        disabled={false}
+      />
+    </ButtonsGroup>
+  );
 };
 
 const AppHeaderShortcutHandlers: React.FC = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <SearchShortcutHandlers
-                action={() => {
-                    navigate(routeSearch);
-                    const searchBox = document.querySelector("#search-box") as HTMLInputElement | null;
-                    searchBox?.focus();
-                }}
-            />
-        </>
-    );
+  return (
+    <>
+      <SearchShortcutHandlers
+        action={() => {
+          navigate(routeSearch);
+          const searchBox = document.querySelector(
+            "#search-box",
+          ) as HTMLInputElement | null;
+          searchBox?.focus();
+        }}
+      />
+    </>
+  );
 };
 
 interface AppHeaderProps {
-    location?: Location;
+  location?: Location;
 }
 
 const headerStyles: React.CSSProperties = {
-    color: "var(--primary)",
+  color: "var(--primary)",
 } as React.CSSProperties;
 
 export const AppHeader: React.FC<AppHeaderProps> = ({ location }) => (
-    <header className="app-header pointer-events-none sticky left-0 right-0 top-8 z-50 flex items-start justify-between px-4 py-0" style={headerStyles}>
-        <AppHeaderShortcutHandlers />
-        <div className="app-header--a flex items-center">
-            <AppMenuNavigation location={location} />
-        </div>
-        <div className="app-header--b flex items-center">
-            <AppMenuActions location={location} />
-        </div>
-    </header>
+  <header
+    className="app-header pointer-events-none sticky left-0 right-0 top-8 z-50 flex items-start justify-between px-4 py-0"
+    style={headerStyles}
+  >
+    <AppHeaderShortcutHandlers />
+    <div className="app-header--a flex items-center">
+      <AppMenuNavigation location={location} />
+    </div>
+    <div className="app-header--b flex items-center">
+      <AppMenuActions location={location} />
+    </div>
+  </header>
 );

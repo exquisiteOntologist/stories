f812ad6f6eaab518bf26149f80948105c642c294
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2025-12-01 16:14:42 +0800

    Don't reserve vertical space for nested collections when there are none

--- stories/src/components/templates/collection/index.tsx
+++ stories/src/components/templates/collection/index.tsx
@@ -44,7 +44,6 @@ import {
   useResetThemeColours,
   useTitle,
 } from "./hooks";
-import ListingsContainer from "../../molecules/listings/listings-container";
 
 const clientItemsLimit: number = 100;
 
@@ -128,7 +127,6 @@ const CollectionView: React.FC<CollectionViewProps> = () => {
         refreshAction={() => setDoRefresh(true)}
         refreshPossible={refreshPossible}
       />
-      {/*<CombinedCount collectionId={collectionId} key={contents?.[0]?.id ?? "article-count"} /> */}
       <div className="flex mb-8">
         <FilterButton
           number={marks.length}
@@ -152,12 +150,14 @@ const CollectionView: React.FC<CollectionViewProps> = () => {
           active={isViewModeActive(filter, ViewMode.PHRASES)}
         />
       </div>
-      <ListingsContainerCollections
-        className="mb-12"
-        view={collectionSettings?.layout as SettingsLayout}
-        collections={nestedCollections}
-        selectAction={(c) => dispatch(chooseCollection(c.id))}
-      />
+      {!!nestedCollections.length && (
+        <ListingsContainerCollections
+          className="mb-12"
+          view={collectionSettings?.layout as SettingsLayout}
+          collections={nestedCollections}
+          selectAction={(c) => dispatch(chooseCollection(c.id))}
+        />
+      )}
       {isViewModeActive(filter, ViewMode.PHRASES) && (
         <ListingsContainerPhrase
           view={collectionSettings?.layout as SettingsLayout}
